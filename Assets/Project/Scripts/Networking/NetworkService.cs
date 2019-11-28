43257427908ba15126b1f5a55d8cb9b266e04161
Shubham <40598861+vijit101@users.noreply.github.com>
2019-11-29 00:00:44 +0530

    -Added basic code for Server

---
+++ Assets/Project/Scripts/Networking/NetworkService.cs
@@ -0,0 +1,25 @@
+﻿using SocketIO;
+using UnityEngine;
+[RequireComponent(typeof(SocketIOComponent))]
+public class NetworkService : MonoBehaviour
+{
+    public SocketIOComponent socketIOComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        socketIOComponent.Connect();
+        socketIOComponent.On("connection", Connections);
+
+        socketIOComponent.On("error", OnError);
+    }
+
+    private void OnError(SocketIOEvent obj)
+    {
+        Debug.Log("Error" + obj);
+    }
+
+    void Connections(SocketIOEvent socketIOEvent)
+    {
+        Debug.Log("Server Connected" + socketIOEvent);
+    }
+}
