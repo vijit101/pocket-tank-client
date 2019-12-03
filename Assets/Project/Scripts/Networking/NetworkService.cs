040dc69cd8eecf040440b460729fbd1a208e3c67
Shubham <40598861+vijit101@users.noreply.github.com>
2019-12-03 23:50:46 +0530

    -Restructured code

--- Assets/Project/Scripts/Networking/NetworkService.cs
+++ Assets/Project/Scripts/Networking/NetworkService.cs
@@ -14,37 +14,41 @@ namespace PocketTanks.Networking
             connectionStatus = ConnectionStatus.UnConnected;
             PlayerPrefs.SetInt(KeyStrings.PlayerID, 0);
             socketIOComponent.Connect();
-            socketIOComponent.On("connection", Connections);
+            socketIOComponent.On("connection", OnConnection); // "Event",Function
             socketIOComponent.On("error", OnError);
         }
 
         private void Update()
         {
         }
+        
+        
+        private void OnError(SocketIOEvent obj)
+        {
+            Debug.Log("NetworkService + Error" + obj);
+        }
+
+        void OnConnection(SocketIOEvent socketIOEvent)
+        {
+            Debug.Log("NetworkService + Server Connected" + socketIOEvent);
+            connectionStatus = ConnectionStatus.Connected;
+            AuthentictionRequest();
+        }
+
         private void AuthentictionRequest()
         {
             if (connectionStatus == ConnectionStatus.Connected)
             {
                 JSONObject PlayerData = new JSONObject();
                 PlayerData[KeyStrings.PlayerID] = new JSONObject(PlayerPrefs.GetInt(KeyStrings.PlayerID));
-                socketIOComponent.On(KeyStrings.AuthenticationRequest, AuthentictionResponse);
-                socketIOComponent.Emit(KeyStrings.AuthenticationRequest, PlayerData);
+                socketIOComponent.On(KeyStrings.AuthenticationRequest, OnAuthenticationResponse);
+                //socketIOComponent.Emit(KeyStrings.AuthenticationRequest, PlayerData);
+                socketIOComponent.Emit(KeyStrings.AuthenticationRequest);
             }
         }
-        private void AuthentictionResponse(SocketIOEvent socketIOEvent)
+        private void OnAuthenticationResponse(SocketIOEvent socketIOEvent)
         {
             Debug.Log("NetworkService + Server Connected" + socketIOEvent);
-        }
-        private void OnError(SocketIOEvent obj)
-        {
-            Debug.Log("NetworkService + Error" + obj);
-        }
-
-        void Connections(SocketIOEvent socketIOEvent)
-        {
-            Debug.Log("NetworkService + Server Connected" + socketIOEvent);
-            connectionStatus = ConnectionStatus.Connected;
-            AuthentictionRequest();
         }
     }
 
